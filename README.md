6df4680e1591f43b53ecdaf1e138f1ce495d84ca
tnichols <tnichols@student.dalat.org>
2022-05-19 22:54:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Obsidian molecule renderer
---
+++ main.ts
@@ -0,0 +1,42 @@
+import { Plugin, MarkdownRenderChild, MarkdownRenderer, PluginSettingTab, App, MarkdownPostProcessorContext } from 'obsidian';
+import { SettingItem, display, loadSettings, saveSettings } from 'obsidian-settings/settings'
+
+export interface ColumnSettings {
+}
+
+const DEFAULT_SETTINGS: ColumnSettings = {
+}
+
+export default class ObsidianColumns extends Plugin {
+
+	async onload() {
+
+		await this.loadSettings();
+		this.addSettingTab(new ObsidianColumnsSettings(this.app, this));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		loadSettings(this, DEFAULT_SETTINGS)
+	}
+
+	async saveSettings() {
+		await saveSettings(this, DEFAULT_SETTINGS)
+	}
+}
+
+class ObsidianColumnsSettings extends PluginSettingTab {
+	plugin: ObsidianColumns;
+
+	constructor(app: App, plugin: ObsidianColumns) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		display(this, DEFAULT_SETTINGS)
+	}
+}
